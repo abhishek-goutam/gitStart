e8a3eb2dfedf36294324d00887bd421e38a0d453
abhishek-goutam <abhishekgoutam8894@gmail.com>
2022-12-03 19:03:44 +0530

    getElementById

--- demo.js
+++ demo.js
@@ -1,2 +1,27 @@
-console.log("I Love Git");
-console.log("I want to learn in depth");
+// console.log("I Love Git");
+// console.log("I want to learn in depth");
+
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.domain);
+// console.log(document.title);
+// console.log(document.all)
+// console.log(document.images);
+// console.log(document.links);
+// console.log(document.all[10])
+
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+// headerTitle.textContent ="Hello";
+// headerTitle.innerHTML = "GoodBye!";
+header.style.borderBottom = 'solid 3px #000'
+// headerTitle.style.borderBottom = 'solid 3px #000'
+// var itemHeader = document.getElementsByTagName('h2');
+
+var items = document.getElementsByClassName('list-group-item');
+
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4 px #ccc';
+header.style.color = 'green';
+
+
