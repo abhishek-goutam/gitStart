f519d710fda1b709f74c4eeb8ff0d3ac0ea58a82
abhishek-goutam <abhishekgoutam8894@gmail.com>
2022-12-03 19:09:10 +0530

    querySelector updated

--- demo.js
+++ demo.js
@@ -11,17 +11,18 @@
 // console.log(document.all[10])
 
 var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
+// var header = document.getElementById('main-header');
 // headerTitle.textContent ="Hello";
 // headerTitle.innerHTML = "GoodBye!";
-header.style.borderBottom = 'solid 3px #000'
+// header.style.borderBottom = 'solid 3px #000'
 // headerTitle.style.borderBottom = 'solid 3px #000'
 // var itemHeader = document.getElementsByTagName('h2');
 
 var items = document.getElementsByClassName('list-group-item');
 
-var header = document.querySelector('#main-header');
-header.style.borderBottom = 'solid 4 px #ccc';
+var header = document.querySelector('.title');
+header.style.borderBottom = 'solid 4 px black';
 header.style.color = 'green';
+header.style.fontWeight ='bold';
 
 
